eeb59e54fff6d552ae1aa71cd77324007e019d30
Isuckatthis <36561366+SXDCGFVBHJN@users.noreply.github.com>
2024-04-18 20:28:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,130 +1,198 @@
-import pygame
-import math
-pygame.init()
-pygame.display.set_caption("ship game")
+#import pygame library
+import pygame  
+#import math library    
+import math  
+#initialize pygame modules       
+pygame.init()      
+#set window caption
+pygame.display.set_caption("ship game")   
+#set window size    
 ssize = [1200, 600]
+#display window to the screen
 screen = pygame.display.set_mode(ssize)
+#get font from system 
 font = pygame.font.SysFont("Cambria", 100)
+# color for background (RGB, light blue)
 colorbg = (0,157,196)
+#create clock
 clock = pygame.time.Clock()
+#game FPS
 FPS = 60
 
-
+# Create button class 
 class Button():
-    
+    #define the init function and get self, image and position
     def __init__(self, image, pos):
-        self.image = image.convert_alpha()
-        self.x = pos[0]
-        self.y = pos[1]
-        self.rect = self.image.get_rect(center=(self.x, self.y))
-        
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-        
-    def interact(self,posi):
-        if posi[0] in range(self.rect.left, self.rect.right) and posi[1] in range(self.rect.top, self.rect.bottom):
-            return True
-        return False
-class ship(pygame.sprite.Sprite):
-    
-    def __init__(self, image, pos, maxspeed, accel, rotosp, maxreverse):        
+        #set self image as the image parameter 
         self.image = image
+        #set x coordinate as the first index in the position list
         self.x = pos[0]
+        #set y coordinate as the second index in the position list
         self.y = pos[1]
-        self.rotosp = rotosp
+        #create a rectangle for the image representing the position
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+    #define the interact function and gets self and the mouse position   
+    def interact(self,mouspos):
+        #check the position x and y of the mouse if it's in the button rectangle 
+        if mouspos[0] in range(self.rect.left, self.rect.right) and mouspos[1] in range(self.rect.top, self.rect.bottom):
+            #return True if the requirements are met
+            return True
+#Creat ship class
+class ship(pygame.sprite.Sprite):
+    #define the init fuction and takes in self, image and position 
+    def __init__(self, image, pos):
+        #set the self image as the image parameter        
+        self.image = image
+        #set self x as the first index of the position list
+        self.x = pos[0]
+        #set self y as the second index of the position list
+        self.y = pos[1]
+        #set rotation speed
+        self.rotosp = 1.5
+        #set current speed
         self.speed = 0
+        #set current angle
         self.angle = 90
-        self.accel = accel
-        self.maxspeed = maxspeed
-        self.maxreverse = maxreverse
-        size = int(self.image.get_width() * 0.1), int(self.image.get_height() * 0.1)
-        self.resizedim = pygame.transform.scale(self.image, size)
-        
-    def rotateim(self, top_left):
-        self.rimage = pygame.transform.rotate(self.resizedim, self.angle)
-        rect= self.rimage.get_rect(center = self.rimage.get_rect(topleft = top_left).center)
-        screen.blit(self.rimage, rect.topleft) 
-        
+        #set acceleration
+        self.accel = 1
+        #set max speed
+        self.maxspeed = 10
+        #set max reverse speed
+        self.maxreverse = -3
+    #define draw function
     def draw(self):
-        self.rotateim((self.x, self.y))
-        
+        #set the size of the ship as the interger result of the default size times 0.1
+        size = int(self.image.get_width() * 0.1), int(self.image.get_height() * 0.1)
+        #transform the image into the size above
+        self.resizedim = pygame.transform.scale(self.image, size)
+        #set the rotated image as the resized image and the angle
+        self.rimage = pygame.transform.rotate(self.resizedim, self.angle)
+        #show the rotated image to the screen
+        screen.blit(self.rimage, [self.x, self.y]) 
+    #define the move around function   
     def movearoun(self):
+        #transform the angle into radians using math library
         rad = math.radians(self.angle)
+        #getting the x and y coordinates by subtracting the value of the sin for x and cos for y times current speed 
         self.x -= math.sin(rad)* self.speed
         self.y -= math.cos(rad)* self.speed
-        
-    def colli(self, mask,x = 0, y = 0 ):
+    #define collision function    
+    def colli(self, mask):
+        #set the mask as the rotated image
         self.mask = pygame.mask.from_surface(self.rimage)
-        offset = [int(self.x - x),int(self.y - y)]
+        #set the offset as the integer of the coordinates
+        offset = [int(self.x),int(self.y)]
+        #set collision to check for the point of intersection
         coll = mask.overlap(self.mask, offset)
+        #return the collision variable
         return coll
-    
+    #define beach function
     def beach(self):
+        #set speed as negative speed
         self.speed = -self.speed
-        self.rotosp = -self.rotosp
+        #call out the movearound function
         self.movearoun()
-        
+    #define input function and takes in the player and key
     def input(player, key):
+        #set movement as false
         movement = False
+        #create a dictionary to map arrow keys to rotation directions and set the corresponding key to the value -1 and 1
         rotation = {pygame.K_LEFT: 1, pygame.K_RIGHT: -1}
+        #iterate through key value in the rotation dictionary
         for direction_key, direction in rotation.items():
+            #check if the corresponding arrow key is pressed
             if key[direction_key]:
-                player.angle += player.rotosp * direction * (player.speed == 0 and 0.2 or 1)
-                movement = True
+                    #check if speed = 0
+                    if player.speed == 0:
+                    #slower rotation
+                        player.angle += player.rotosp * direction/5
+                    else:
+                    # Standard rotation
+                        player.angle += player.rotosp * direction
+        #check if key is the up arrow key
         if key[pygame.K_UP]:
+            #change movement to True
             movement = True
+            #set the speed as the minimum value between the current speed + acceleration and max speed
             player.speed = min(player.speed + player.accel, player.maxspeed)
+        #check if key is the down arrow key
         elif key[pygame.K_DOWN]:
+            #change movement to True
             movement = True
+            #set the speed as the maximum value between the current speed - acceleration and max reverse speed
             player.speed = max(player.speed - player.accel, player.maxreverse)
-        if not movement:
+        #check if movement is False
+        if movement == False:
+            #check if speed is above 0 
             if player.speed > 0:
-                player.speed = max(player.speed - 0.25, 0)
+                #set the speed as the maximum value between the current speed - 1 and 0
+                player.speed = max(player.speed - 1, 0)
             else:
-                player.speed = min(player.speed + 0.25, 0)
-        if movement:
-            player.movearoun()
-                                                     
+                #set the speed as the minimum value between the current speed + 1 and 0
+                player.speed = min(player.speed + 1, 0)
+        #calling out the movearound function
+        player.movearoun()
+#render text "SHIP GAME" onto a surface using the font variable, antialiasing, and black color
 menutext = font.render("SHIP GAME", True, "#000000")
-
+#rectangle representing the position of the rendered text
 menurect = menutext.get_rect(center=(600,100))
-
+#Create a Button object representing the "play" button
 play = Button(image=pygame.image.load("img/play.png"), pos=[600,250])
-
+#Create a Button object representing the "quit" button
 quit = Button(image=pygame.image.load("img/QUIT.png"), pos=[600,400])
-
-p = ship(image = (pygame.image.load("img/ship.png")), pos = [600,300], maxspeed = 5, accel = 0.5, rotosp = 1.5, maxreverse = -3)
-islands = pygame.image.load("img/islands.png").convert_alpha()
-
+#Create a ship object representing the player
+p = ship(image = (pygame.image.load("img/ship.png")), pos = [600,300])
+#load the islands image
+islands = pygame.image.load("img/islands.png")
+#get mask from the islands image
 islandsmask = pygame.mask.from_surface(islands)
+#set running as True
 running = True
+#set gamestate as menu
 gamestate = "menu"
-
+# main script 
 while running:
-    
+    # fill the screen with the color light blue 
     screen.fill(colorbg)
+    #check if the gamestate is menu
     if gamestate == "menu": 
-        play.draw(screen)
-        quit.draw(screen)
+        #display the button image and the menu text to the screen and the rect as its position
+        screen.blit(play.image, play.rect)
+        screen.blit(quit.image, quit.rect)
         screen.blit(menutext, menurect)
-        
+    #check if the gamestate is play   
     if gamestate == "play":
+        #p calling out draw function
         p.draw()
-        p.input(key = pygame.key.get_pressed())  
+        #p calling out the input function and takes in the key variable as it uses pygame to detect the key pressed
+        p.input(key = pygame.key.get_pressed())
+        #display the island picture to the screen at the coordinates 0,0  
         screen.blit(islands, [0,0])            
+        #check if there's any collision between the island mask and the ship mask
         if p.colli(islandsmask):
+        #calling out the beach fuction when met
             p.beach()    
-        
+    #iterate the events in every events that pygame gets  
     for event in pygame.event.get():
-        menu_mousepos = pygame.mouse.get_pos()
+        #get the mouse position
+        mousepos = pygame.mouse.get_pos()
+        #check if the event type is mouse click
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if play.interact(menu_mousepos):
+            # check if the mouse click position is in one of the buttons
+            if play.interact(mousepos):
+                #change gamestate to play
                 gamestate = "play"
-            if quit.interact(menu_mousepos):
+            if quit.interact(mousepos):
+                #set running as false
                 running = False
+        #check if the event type is quit
         if event.type == pygame.QUIT:
+            #set running as false
             running = False
 
+    #make the game runs 60 frames per seconds
     clock.tick(FPS)
+    #update the screen
     pygame.display.update()
+#quit pygame
 pygame.quit()
